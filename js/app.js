7b8eb3daf0c0c2f5f0c89caaa7c6e7d241c25959
unknown <hosenzobayer55@gmail.com>
2023-10-20 20:48:06 +0600

    something update

--- js/app.js
+++ js/app.js
@@ -3,3 +3,15 @@ AOS.init({
   offset: 250,
   duration: 500,
 });
+
+$(".reviews_details").slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+// Rotate slider
+$(function () {
+  $(".reviews_details").rotateSlider({});
+});
