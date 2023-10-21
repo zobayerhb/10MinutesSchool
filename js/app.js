1245d80baebc435af66c7424abb1a01ff5bf7440
Zobayer Hosen <hosenzobayer55@gmail.com>
2023-10-21 19:38:02 +0600

    something update

--- js/app.js
+++ js/app.js
@@ -1,17 +1,46 @@
+// Rotate slider
+$(function () {
+  $('.screenshot_wrapper').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    autoplay: true
+  });
+
+  //back to top
+  $(".bottom_top_icon").click(function(){
+    $('html,body').animate({scrollTop:0},1000);
+  })
+  //sticky menu
+  $(window).on('scroll',function(){
+    let main = 0
+     main = $(this).scrollTop();
+    if(main > 250){
+      $(".navbar").addClass("xyz")
+    }else{
+      $(".navbar").removeClass("xyz")
+    }
+    if(main > 300){
+      $('.bottom_top_icon').fadeIn(300);
+    }
+    else{
+      $('.bottom_top_icon').fadeOut(300);
+    }
+
+});
+//preloader
+$(window).on('load',function(){
+  $('.preloader').delay(1000).fadeOut(300);
+});
+    
+});
 // AOS INITIALIZATION
 AOS.init({
   offset: 250,
   duration: 500,
 });
 
-$(".reviews_details").slick({
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-});
 
-// Rotate slider
-$(function () {
-  $(".reviews_details").rotateSlider({});
-});
+
