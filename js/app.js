6aebebba720c49a02be7478d39183e22de70094a
Zobayer Hosen <hosenzobayer55@gmail.com>
2023-10-23 22:52:34 +0600

    file modified

--- js/app.js
+++ js/app.js
@@ -11,7 +11,36 @@ $(window).on('load',function(){
     slidesToScroll: 1,
     arrows: false,
     dots: true,
-    autoplay: true
+    autoplay: true,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 575.98,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
 
   //back to top
@@ -34,6 +63,10 @@ $(window).on('load',function(){
       $('.bottom_top_icon').fadeOut(300);
     }
 });
+
+//Venobox js
+$('.venobox').venobox();
+
 });
 // AOS INITIALIZATION
 AOS.init({
